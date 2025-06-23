a2f42ecd4bc49baf7360b9653e96554ec98f6eba
DaSonOfPoseidon <keithleyjj@icloud.com>
2025-06-22 23:05:10 -0500

    Add PyUpdater config, GitHub Actions workflow, and ignore rules

---
+++ client_config.py
@@ -0,0 +1,7 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'None'
+    APP_NAME = 'TaskScraper'
+    COMPANY_NAME = 'Jackson K'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['https://dasonofposeidon.github.io/TaskScraper/']
