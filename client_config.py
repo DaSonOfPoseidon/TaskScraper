32f4a5188271bb2f43c08c8d255b90fa3b6cc6a5
DaSonOfPoseidon <keithleyjj@icloud.com>
2025-06-27 22:45:02 -0500

    it's .exe time

--- client_config.py
+++
@@ -1,7 +0,0 @@
-class ClientConfig(object):
-    PUBLIC_KEY = 'KU20Ac+N8JEWcUhR667AbODhJjJuf0RoAmLA+QPFPmI'
-    APP_NAME = 'ConsultationCrusher'
-    COMPANY_NAME = 'Jackson K'
-    HTTP_TIMEOUT = 30
-    MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = ['https://dasonofposeidon.github.io/TaskScraper/']
